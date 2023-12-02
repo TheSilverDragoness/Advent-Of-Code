3eed6adadcde7671bee2c9ae68207ea5a83ab850
TheSilverDragoness <hannahemilia01@gmail.com>
2023-12-02 12:15:22 +0000

    2023 day 2 completed

---
+++ Advent Of Code/2023/Day5/Day5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void part1()
+{
+	std::ifstream input("input.txt");
+	std::string str;
+
+	if (input.is_open())
+	{
+
+	}
+}
+
+void part2()
+{
+	std::ifstream input("input.txt");
+	std::string str;
+
+	if (input.is_open())
+	{
+
+	}
+}
+
+int main()
+{
+	return 0;
+}
