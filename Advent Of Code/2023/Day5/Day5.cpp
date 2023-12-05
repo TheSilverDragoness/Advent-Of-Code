d2e852fe199cf9babf33a2c872c222a75c23ffc6
TheSilverDragoness <hannahemilia01@gmail.com>
2023-12-05 14:50:29 +0000

    Started on day 5

--- Advent Of Code/2023/Day5/Day5.cpp
+++ Advent Of Code/2023/Day5/Day5.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 void part1()
 {
 	std::ifstream input("input.txt");
 	std::string str;
-
+	std::vector<int> seeds;
+	bool isseed = true;
+	
 	if (input.is_open())
 	{
-
+		while (getline(input, str)) 
+		{
+			std::istringstream iss(str);
+			std::string token;
+			
+			while (iss >> token)
+			{
+				if (isdigit(token[0]) && isdigit(token.back()) && isseed)
+				{
+					seeds.push_back(stoi(token));
+				}
+			}
+			isseed = false;
+		}
+		std::cout << "seeds: ";
+		for (int i = 0; i < seeds.size(); i++)
+		{
+			if (i < seeds.size())
+				std::cout << seeds[i];
+		}
 	}
 }
 
@@ -26,5 +49,6 @@ void part2()
 
 int main()
 {
+	part1();
 	return 0;
 }
