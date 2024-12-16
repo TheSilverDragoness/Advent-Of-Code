29829bccec1e0a477e818ede6b7dede61f87c6b1
TheSilverDragoness <hannahemilia01@gmail.com>
2024-12-16 19:39:21 +0000

    Start on day 8
    planning out approach

---
+++ AdventOfCode/2024/Day8/Day8.cpp
@@ -0,0 +1,7 @@
+//create antenna class
+//  Includes type (char), and location (pair)
+//walk through map, for each antenna type, check if already encountered, if not, add it to a vector of types
+//new vector for each different antenna, stores each location
+//for each pair of antenna, calculate each location of antinode
+//if location in map, and not in vector of existing antinodes then add it to vector of antinodes;
+
