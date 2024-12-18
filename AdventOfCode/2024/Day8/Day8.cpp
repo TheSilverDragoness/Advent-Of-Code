260c8d33aa5380c7a79177499ad4d29074a3c8df
TheSilverDragoness <hannahemilia01@gmail.com>
2024-12-18 00:55:31 +0000

    Day 8 input parsing done
    bit slow with this one, had a rough day

--- AdventOfCode/2024/Day8/Day8.cpp
+++ AdventOfCode/2024/Day8/Day8.cpp
@@ -1,5 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+std::ifstream input("input.txt");
+std::string str;
+
+void Part1()
+{
+    std::vector<std::vector<char>> grid;
+    std::multimap<char, std::pair<int, int>> antennas;
+    std::vector<std::pair<int, int>> antiNodes;
+
+    int ycoord = 0;
+    int xcoord = 0;
+
+    while (getline(input, str))
+    {
+        for (auto c : str)
+        {
+            if (c == '.')
+            {
+                xcoord++;
+                continue;
+            }
+
+            antennas.emplace(c, std::make_pair(xcoord, ycoord));
+            xcoord++;
+        }
+        ycoord++;
+    }
+}
+
+void Part2()
+{
+    while (getline(input, str))
+    {
+        
+    }
+}
+
+int main()
+{
+    if (!input.is_open())
+    {
+        std::cout << "input is not open";
+        return 1;
+    }
+    Part1();
+    return 0;
+}
+
 //create antenna class
 //  Includes type (char), and location (pair)
+
 //walk through map, for each antenna type, check if already encountered, if not, add it to a vector of types
 //new vector for each different antenna, stores each location
 //for each pair of antenna, calculate each location of antinode
